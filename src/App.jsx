f336547fcc49e8ab5bf6019a57e64a2c8a65f446
Nagilo Araujo <nagiloaraujo@users.noreply.github.com>
2026-03-13 22:15:01 -0300

    Implement n8n chat interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
-function App() {
-  return null
-}
+import { useEffect, useRef } from 'react';
+import '@n8n/chat/style.css';
+import { createChat } from '@n8n/chat';
+import './App.css';
 
-export default App
+export const App = () => {
+	useEffect(() => {
+		createChat({
+			webhookUrl: 'https://n8n.srv1025595.hstgr.cloud/webhook/0b3aba4b-eb27-41f2-88d8-8296ac90553c/chat',
+            mode: 'fullscreen',
+            showWelcomeScreen: true, 
+            initialMessages: [
+                'Olá! 👋',
+                'Como posso ajudar você hoje?'
+            ],
+            i18n: {
+                en: {
+                    title: 'N8N Chat Test',
+                    subtitle: 'Teste de integração com N8N',
+                    footer: 'Powered by n8n',
+                    getStarted: 'Nova Conversa',
+                    inputPlaceholder: 'Digite sua mensagem...',
+                },
+            },
+		});
+	}, []);
+
+	return (<div></div>);
+};
+
+export default App;
